050711092cdbbe8802ce4fa8fd7a197df191e385
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-07-05 06:30:54 +0900

    コンストラクタについて学んだ

---
+++ constructor.swift
@@ -0,0 +1,109 @@
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+
+
+    init(name: String) {
+       self.name = name
+    }
+
+
+    func simpleDescription() -> String {
+       return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+
+    var perimeter: Double {
+        get {
+             return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+
+
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+
+
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+// Prints "10.0"
+print(triangleAndSquare.triangle.sideLength)
+// Prints "10.0"
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
+// Prints "50.0"
+
+class Sakuma {
+    var age: Int {
+        
+        willSet {
+            print("willSet:\(self.age)")
+        }
+        
+        didSet {
+            print("didSet:\(self.age)")
+        }
+    }
+
+    
+    init(age: Int) {
+        self.age = age
+    }
+    
+}
+
+var sakuma = Sakuma(age: 31)
+print(sakuma.age)
+sakuma.age = 32
